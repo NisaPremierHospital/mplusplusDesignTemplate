d6ec8dcbe5ed9fd91dcbb6548eda34cfa71021d1
daaef <realafe21@gmail.com>
2018-06-30 19:09:40 +0100

    made the modals unique

--- src/components/Report.js
+++ src/components/Report.js
@@ -9,18 +9,23 @@ class Report extends Component {
     super(props);
     this.state = {
       cards: [
-        {id: 1, link: '#modal-center', icon: 'folder open', title: 'Registration', width: 'uk-width-1-3@m', desc: 'Register Patient' +
+        {id: 1, link: '#regmod', icon: 'folder open', title: 'Registration', width: 'uk-width-1-3@m', desc: 'Register' +
+          ' Patient' +
           ' Record or Search for existing patients'},
-        {id: 2, link: '/', icon: 'user doctor', title: 'Consultation', width: 'uk-width-1-3@m', desc: 'Get information from' +
+        {id: 2, link: '#consultmod', icon: 'user doctor', title: 'Consultation', width: 'uk-width-1-3@m', desc: 'Get' +
+          ' information from' +
           ' doctors or assigned representatives'},
-        {id: 3, link: '/', icon: 'heartbeat', title: 'Admissions & Discharge', width: 'uk-width-1-3@m', desc: 'Messages and Notifications to patients'},
-        {id: 4, link: '/', icon: 'eye dropper', title: 'Immunization', width: 'uk-width-1-3@m', desc: 'Schedule' +
+        {id: 3, link: '#addismod', icon: 'heartbeat', title: 'Admissions & Discharge', width: 'uk-width-1-3@m', desc: 'Messages and Notifications to patients'},
+        {id: 4, link: '#immunemod', icon: 'eye dropper', title: 'Immunization', width: 'uk-width-1-3@m', desc: 'Schedule' +
           ' patient for appointment and other events'},
-        {id: 5, link: '/', icon: 'lab', title: 'Laboratory', width: 'uk-width-1-3@m', desc: 'Laboratory Requests/Results'},
-        {id: 6, link: '/', icon: 'pills', title: 'Pharmacy', width: 'uk-width-1-3@m', desc: 'Drug Prescriptions for Patients'},
-        {id: 7, link: '/', icon: 'medkit', title: 'Theatre', width: 'uk-width-1-3@m', desc: 'Patients Radiological Documents'},
-        {id: 8, link: '/', icon: 'wait', title: 'Waiting Time', width: 'uk-width-1-3@m', desc: 'Patients Procedures'},
-        {id: 9, link: '/', icon: 'thermometer empty', title: 'Radiology', width: 'uk-width-1-3@m', desc: 'Manage' +
+        {id: 5, link: '#labmod', icon: 'lab', title: 'Laboratory', width: 'uk-width-1-3@m', desc: 'Laboratory' +
+          ' Requests/Results'},
+        {id: 6, link: '#pharmmod', icon: 'pills', title: 'Pharmacy', width: 'uk-width-1-3@m', desc: 'Drug Prescriptions for' +
+          ' Patients'},
+        {id: 7, link: '#theatremod', icon: 'medkit', title: 'Theatre', width: 'uk-width-1-3@m', desc: 'Patients Radiological' +
+          ' Documents'},
+        {id: 8, link: '#waitmod', icon: 'wait', title: 'Waiting Time', width: 'uk-width-1-3@m', desc: 'Patients Procedures'},
+        {id: 9, link: '#radiomod', icon: 'thermometer empty', title: 'Radiology', width: 'uk-width-1-3@m', desc: 'Manage' +
           ' Patients admissions/discharge'}
       ]
     }
@@ -36,10 +41,44 @@ class Report extends Component {
           })}
         </section>
   
-        <div id="modal-center" className="uk-flex-middle" data-uk-modal>
+        {/*Modal for Registration*/}
+        <div id="regmod" className="uk-flex-middle" data-uk-modal>
           <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
       
             <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Registration</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Registration*/}
+        
+        {/*Modal for Consultation*/}
+        <div id="consultmod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Consultation</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Consultation*/}
+        
+        {/*Modal for Admission & Discharge*/}
+        <div id="addismod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Admission & Discharge</h3>
       
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
               dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
@@ -49,6 +88,109 @@ class Report extends Component {
     
           </div>
         </div>
+        {/*Modal for Admission & Discharge*/}
+        
+        {/*Modal for Immunization*/}
+        <div id="immunemod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Immunization</h3>
+      
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Immunization*/}
+        
+        {/*Modal for Laboratory*/}
+        <div id="labmod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Laboratory</h3>
+      
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Laboratory*/}
+        
+        {/*Modal for Pharmacy*/}
+        <div id="pharmmod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Pharmacy</h3>
+      
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Pharmacy*/}
+        
+        {/*Modal for Theatre*/}
+        <div id="theatremod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Theatre</h3>
+      
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Theatre*/}
+        
+        {/*Modal for Waiting Time*/}
+        <div id="waitmod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Waiting Time</h3>
+      
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Waiting Time*/}
+        
+        {/*Modal for Radiology*/}
+        <div id="radiomod" className="uk-flex-middle" data-uk-modal>
+          <div className="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      
+            <button className="uk-modal-close-default" type="button" data-uk-close></button>
+            <h3 className="uk-heading-primary">Radiology</h3>
+      
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.</p>
+    
+          </div>
+        </div>
+        {/*Modal for Radiology*/}
       </header>
     );
   }
